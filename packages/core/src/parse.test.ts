1f5ac73883062e3383ed3f53060e1cff98d203e3
taggon <gonom9@gmail.com>
2023-11-07 00:00:06 +0900

    Done with initial version of wongoji core

---
+++ packages/core/src/parse.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from 'bun:test';
+import { parse } from './parse';
+
+const testCases = [
+  '안녕하세요. 반갑습니다.',
+  '대한민국은 Korea이다.',
+  '걸음도 해깝고 방울소리가 밤 벌판에 한층 청청하게 울렸다.\n달이 어지간히 기울어졌다.',
+  '"이걸 까셀라 부다!"하고 소리를 쳤다.',
+  '대체 이게 웬 속인지(지금까지도 난 영문을 모른다) 아버질 혼내 주기는 제가 내래 놓고 이제 와서는 달려들며, "에그머니! 이 망할 게 아버지 죽이네!" 하고 내 귀를 뒤로 잡아당기며 마냥 우는 것이 아니냐.'
+];
+
+testCases.forEach((text, index) => {
+  test(`Case #${index + 1}: ${text}`, () => {
+    expect(parse(text)).toMatchSnapshot();
+  });
+});
---
+++ packages/core/src/parse.ts
@@ -0,0 +1,146 @@
+import { tokenize, type Token } from './tokenize';
+
+export type Document = {
+  type: 'document';
+  nodes: (Paragraph | Quote | SingleQuote)[];
+};
+
+export type Paragraph = {
+  type: 'paragraph';
+  nodes: (FullText | HalfText | Space | Punctuation | Quote | SingleQuote)[];
+};
+
+export type Quote = {
+  type: 'quote';
+  nodes: (FullText | HalfText | Space | Punctuation | SingleQuote)[];
+};
+
+export type SingleQuote = {
+  type: 'singlequote';
+  nodes: (FullText | HalfText | Space | Punctuation)[];
+};
+
+export type Space = {
+  type: 'space';
+};
+
+export type FullText = {
+  type: 'fulltext';
+  raw: string;
+};
+
+export type HalfText = {
+  type: 'halftext';
+  raw: string;
+};
+
+export type Punctuation = {
+  type: 'punctuation';
+  raw: string;
+};
+
+type ParentNode = Document | Paragraph | Quote | SingleQuote;
+
+function eat(tokens: Token[], types: Token['type'][], start: number) {
+  let i = start - 1;
+
+  while (types.includes(tokens[++i]?.type));
+
+  return i - 1;
+}
+
+function parseTokensIntoNodes(parent: ParentNode, tokens: Token[], from: number, to: number) {
+  let i = from - 1;
+
+  while(++i < tokens.length && i < to) {
+    const tok = tokens[i];
+
+    switch (tok.type) {
+      case 'fulltext':
+      case 'halftext':
+      case 'punctuation':
+        // 새 문단을 시작한다.
+        if (parent.type === 'document') {
+          const start = i;
+          const endOfParagraph = tokens.findIndex((t, idx) => {
+            return idx > i && t.type === 'newline';
+          });
+          i = endOfParagraph === -1 ? tokens.length : endOfParagraph;
+
+          const paragraph: Paragraph = { type: 'paragraph', nodes: [] };
+          parseTokensIntoNodes(paragraph, tokens, start, i);
+          parent.nodes.push(paragraph);
+          break;
+        }
+
+        parent.nodes.push({
+          type: tok.type,
+          raw: tok.raw,
+        });
+
+        // 문장 부호라면 뒤따른 공백을 무시한다.
+        if (tok.type === 'punctuation') {
+          i = eat(tokens, ['space'], i + 1);
+        }
+        break;
+      case 'space':
+        // 최상위 노드에는 공백이 허용되지 않는다.
+        if (parent.type === 'document') continue;
+        if (parent.nodes.length > 0) {
+          parent.nodes.push({ type: 'space' });
+        }
+        break;
+      case 'newline':
+        // 최상위 노드에는 줄바꿈이 허용되지 않는다.
+        if (parent.type === 'document') continue;
+        if (parent.nodes.length > 0) {
+          parent.nodes.push({ type: 'space' });
+        }
+        break;
+      case 'quote':
+        // 닫는 따옴표의 인덱스를 찾는다.
+        const start = i + 1;
+        const closingQuoteIndex = tokens.findIndex((t, idx) => {
+          return idx > i && t.type === 'quote' && t.raw === tok.raw;
+        });
+        i = closingQuoteIndex;
+
+        if (closingQuoteIndex === -1) {
+          // TODO: 닫는 따옴표를 찾지 못했을 때의 에러 처리
+        } else {
+          const quote: Quote | SingleQuote = { type: tok.raw === '"' ? 'quote' :  'singlequote', nodes: [] };
+          parseTokensIntoNodes(quote, tokens, start, closingQuoteIndex);
+
+          // 문단을 따옴표로 시작했지만, 보통 문단의 일부인 경우
+          if (parent.type === 'document') {
+            i = eat(tokens, ['space'], i + 1);
+            if (['fulltext', 'halftext'].includes(tokens[i + 1]?.type)) {
+              const paragraph: Paragraph = { type: 'paragraph', nodes: [quote] };
+              parent.nodes.push(paragraph);
+
+              // 뒤이어 나오는 문장을 같은 문단으로 처리한다.
+              parent = paragraph;
+              continue;
+            }
+          }
+
+          if (parent.type === 'quote' || parent.type === 'singlequote') {
+            // 따옴표가 중첩되는 경우가 아니면 여기를 탈 일이 없을텐데...
+            break;
+          }
+
+          parent.nodes.push(quote);
+        }
+        break;
+    }
+  }
+}
+
+export function parse(text: string): Document {
+  const tokens = tokenize(text);
+  const doc: Document = { type: 'document', nodes: [] };
+
+  parseTokensIntoNodes(doc, tokens, 0, tokens.length);
+
+  return doc;
+}
